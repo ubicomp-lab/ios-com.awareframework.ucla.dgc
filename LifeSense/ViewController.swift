6edeca59bd614e8169295d23150e0e94dd84ff34
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2018-03-26 10:35:59 +0300

    Initial Commit

---
+++ LifeSense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeSense
+//
+//  Created by Yuuki Nishiyama on 2018/03/26.
+//  Copyright © 2018 Yuuki Nishiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
